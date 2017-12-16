108b9bc58bf948abe12571952c60d0fa1252e8a7
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2017-12-16 19:29:04 +0100

    Create a class within ForecastAdapter called ForecastAdapterViewHolder

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,8 +1,48 @@
 package com.example.android.sunshine;
 
+import android.support.v7.widget.RecyclerView;
+
 /**
  * Created by jean-marc on 16/12/17.
  */
 
 public class ForecastAdapter {
+
+    // Within ForecastAdapter.java /////////////////////////////////////////////////////////////////
+    // completed (15) Add a class file called ForecastAdapter
+    // TODO (22) Extend RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder>
+
+    // TODO (23) Create a private string array called mWeatherData
+
+    // TODO (47) Create the default constructor (we will pass in parameters in a later lesson)
+
+    // completed (16) Create a class within ForecastAdapter called ForecastAdapterViewHolder
+    // completed (17) Extend RecyclerView.ViewHolder
+    class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        // Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
+        // TODO (18) Create a public final TextView variable called mWeatherTextView
+
+        // TODO (19) Create a constructor for this class that accepts a View as a parameter
+        // TODO (20) Call super(view) within the constructor for ForecastAdapterViewHolder
+        // TODO (21) Using view.findViewById, get a reference to this layout's TextView and save it to mWeatherTextView
+        // Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
+
+    }
+
+
+    // TODO (24) Override onCreateViewHolder
+    // TODO (25) Within onCreateViewHolder, inflate the list item xml into a view
+    // TODO (26) Within onCreateViewHolder, return a new ForecastAdapterViewHolder with the above view passed in as a parameter
+
+    // TODO (27) Override onBindViewHolder
+    // TODO (28) Set the text of the TextView to the weather for this list item's position
+
+    // TODO (29) Override getItemCount
+    // TODO (30) Return 0 if mWeatherData is null, or the size of mWeatherData if it is not null
+
+    // TODO (31) Create a setWeatherData method that saves the weatherData to mWeatherData
+    // TODO (32) After you save mWeatherData, call notifyDataSetChanged
+    // Within ForecastAdapter.java /////////////////////////////////////////////////////////////////
+
 }
