3951a8e075a6a811e75309e186a0504fdcb65e70
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2017-12-17 12:47:11 +0100

    Override getItemCount: return 0 if mWeatherData is null, or the size of mWeatherData if it is not null

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -44,8 +44,16 @@ public class ForecastAdapter
         holder.mWeatherTextView.setText(String.valueOf(position));
     }
 
-    // TODO (29) Override getItemCount
-    // TODO (30) Return 0 if mWeatherData is null, or the size of mWeatherData if it is not null
+    // completed (29) Override getItemCount
+    // completed (30) Return 0 if mWeatherData is null, or the size of mWeatherData if it is not null
+    @Override
+    public int getItemCount() {
+        if (mWeatherData == null) {
+            return 0;
+        } else {
+            return mWeatherData.length;
+        }
+    }
 
     // TODO (31) Create a setWeatherData method that saves the weatherData to mWeatherData
     // TODO (32) After you save mWeatherData, call notifyDataSetChanged
