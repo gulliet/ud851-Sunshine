648aa5076b74664d94aa79b34747b43a617df654
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2017-12-23 18:35:50 +0100

    Remove the Toast and launch the DetailActivity using an explicit Intent

--- S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.sunshine;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -119,9 +120,9 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
         Context context = this;
         // completed (1) Create a new Activity called DetailActivity using Android Studio's wizard
         // completed (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
-        // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
-        Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
-                .show();
+        // completed (3) Remove the Toast and launch the DetailActivity using an explicit Intent
+        Intent intent = new Intent(context, DetailActivity.class);
+        startActivity(intent);
     }
 
     /**
