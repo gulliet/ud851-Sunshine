df2990d356a0595f1ae955fd95eb489b195ce840
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-16 20:02:51 +0100

    Implement OnSharedPreferenceChangeListener from SettingsFragment

--- S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
+++ S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.example.android.sunshine;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
@@ -10,14 +12,26 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 
 // completed (4) Create SettingsFragment and extend PreferenceFragmentCompat
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     // Do steps 5 - 11 within SettingsFragment
-    // TODO (10) Implement OnSharedPreferenceChangeListener from SettingsFragment
+    // completed (10) Implement OnSharedPreferenceChangeListener from SettingsFragment
 
     // completed (8) Create a method called setPreferenceSummary that accepts a Preference and an Object and sets the summary of the preference
     private void setPreferenceSummary(Preference preference, Object value) {
+        String stringValue = value.toString();
+        String key = preference.getKey();
 
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            } else {
+                preference.setSummary(stringValue);
+            }
+        }
     }
 
     // TODO (13) Unregister SettingsFragment (this) as a SharedPreferenceChangedListener in onStop
@@ -32,7 +46,12 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         addPreferencesFromResource(R.xml.pref_general);
 
         // Do step 9 within onCreatePreference
-        // TODO (9) Set the preference summary on each preference that isn't a CheckBoxPreference
+        // completed (9) Set the preference summary on each preference that isn't a CheckBoxPreference
+
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
 
     }
 }
