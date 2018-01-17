a7fa552e84658be4f5dfa4055089fce4bf68f730
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-17 19:06:56 +0100

    Increment the database version after changing the create table statement

--- S07.03-Exercise-ConflictResolutionPolicy/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ S07.03-Exercise-ConflictResolutionPolicy/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -32,7 +32,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
      */
     public static final String DATABASE_NAME = "weather.db";
 
-//  TODO (2) Increment the database version after changing the create table statement
+//  completed (2) Increment the database version after changing the create table statement
     /*
      * If you change the database schema, you must increment the database version or the onUpgrade
      * method will not be called.
@@ -44,7 +44,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
      * use-case, we wanted to watch out for it and warn you what could happen if you mistakenly
      * version your databases.
      */
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
 
     public WeatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
