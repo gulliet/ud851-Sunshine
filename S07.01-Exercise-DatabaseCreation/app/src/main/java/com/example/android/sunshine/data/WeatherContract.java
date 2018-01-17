9d5f9e21b7d70a35592de3b70e79195bf3202512
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-17 15:13:06 +0100

    chore: Clean up code organization and layout.

--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -23,28 +23,21 @@ import android.provider.BaseColumns;
  */
 public class WeatherContract {
     // Prevent accidental instantiation of the contract class.
-    private WeatherContract() {}
+    private WeatherContract() {
+    }
 
-//  completed (1) Within WeatherContract, create a public static final class called WeatherEntry that implements BaseColumns
+    //  completed (1) Within WeatherContract, create a public static final class called WeatherEntry that implements BaseColumns
     public static final class WeatherEntry implements BaseColumns {
 
-//      Do steps 2 through 10 within the WeatherEntry class
-
-//      TODO (2) Create a public static final String call TABLE_NAME with the value "weather"
-
-//      TODO (3) Create a public static final String call COLUMN_DATE with the value "date"
-
-//      TODO (4) Create a public static final String call COLUMN_WEATHER_ID with the value "weather_id"
-
-//      TODO (5) Create a public static final String call COLUMN_MIN_TEMP with the value "min"
-//      TODO (6) Create a public static final String call COLUMN_MAX_TEMP with the value "max"
-
-//      TODO (7) Create a public static final String call COLUMN_HUMIDITY with the value "humidity"
-
-//      TODO (8) Create a public static final String call COLUMN_PRESSURE with the value "pressure"
-
-//      TODO (9) Create a public static final String call COLUMN_WIND_SPEED with the value "wind"
-
-//      TODO (10) Create a public static final String call COLUMN_DEGREES with the value "degrees"
+        // Do steps 2 through 10 within the WeatherEntry class
+        // TODO (2) Create a public static final String call TABLE_NAME with the value "weather"
+        // TODO (3) Create a public static final String call COLUMN_DATE with the value "date"
+        // TODO (4) Create a public static final String call COLUMN_WEATHER_ID with the value "weather_id"
+        // TODO (5) Create a public static final String call COLUMN_MIN_TEMP with the value "min"
+        // TODO (6) Create a public static final String call COLUMN_MAX_TEMP with the value "max"
+        // TODO (7) Create a public static final String call COLUMN_HUMIDITY with the value "humidity"
+        // TODO (8) Create a public static final String call COLUMN_PRESSURE with the value "pressure"
+        // TODO (9) Create a public static final String call COLUMN_WIND_SPEED with the value "wind"
+        // TODO (10) Create a public static final String call COLUMN_DEGREES with the value "degrees"
     }
 }
