9cd676e5d1741990c2263e640495f31ff304b23e
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-21 08:00:15 +0100

    fix: Ensure that delete returns the number of deleted rows

--- S09.03-Exercise-ContentProviderDelete/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
+++ S09.03-Exercise-ContentProviderDelete/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -103,11 +103,11 @@ public class WeatherProvider extends ContentProvider {
      * In onCreate, we initialize our content provider on startup. This method is called for all
      * registered content providers on the application main thread at application launch time.
      * It must not perform lengthy operations, or application startup will be delayed.
-     *
+     * <p>
      * Nontrivial initialization (such as opening, upgrading, and scanning
      * databases) should be deferred until the content provider is used (via {@link #query},
      * {@link #bulkInsert(Uri, ContentValues[])}, etc).
-     *
+     * <p>
      * Deferred initialization keeps application startup fast, avoids unnecessary work if the
      * provider turns out not to be needed, and stops database errors (such as a full disk) from
      * halting application launch.
@@ -135,7 +135,6 @@ public class WeatherProvider extends ContentProvider {
      * @param uri    The content:// URI of the insertion request.
      * @param values An array of sets of column_name/value pairs to add to the database.
      *               This must not be {@code null}.
-     *
      * @return The number of values that were inserted.
      */
     @Override
@@ -294,6 +293,7 @@ public class WeatherProvider extends ContentProvider {
     }
 
 //  completed (1) Implement the delete method of the ContentProvider
+
     /**
      * Deletes data at a given URI with optional arguments for more fine tuned deletions.
      *
@@ -308,13 +308,23 @@ public class WeatherProvider extends ContentProvider {
         SQLiteDatabase db = mOpenHelper.getWritableDatabase();
         int match = sUriMatcher.match(uri);
         int rowsDeleted = 0;
+/*
+       If we pass null as the selection to SQLiteDatabase#delete, our entire table will be
+       deleted. However, if we do pass null and delete all of the rows in the table, we won't
+       know how many rows were deleted. According to the documentation for SQLiteDatabase,
+       passing "1" for the selection will delete all rows and return the number of rows
+       deleted, which is what the caller of this method expects.
+*/
+        if (selection == null) {
+            selection = "1";
+        }
 
 //          completed (2) Only implement the functionality, given the proper URI, to delete ALL rows in the weather table
         switch (match) {
             case CODE_WEATHER:
                 rowsDeleted = db.delete(WeatherContract.WeatherEntry.TABLE_NAME,
-                        null,
-                        null);
+                        selection,
+                        selectionArgs);
                 break;
 
             default:
