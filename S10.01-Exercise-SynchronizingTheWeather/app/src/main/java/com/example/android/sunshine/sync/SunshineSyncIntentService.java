3940c328085c60c882b4aca8fd526c6dd012c32d
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-27 11:44:33 +0100

    Create a new class called SunshineSyncIntentService that extends IntentService

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -1,3 +1,21 @@
-// TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
+package com.example.android.sunshine.sync;
+
+// completed (5) Create a new class called SunshineSyncIntentService that extends IntentService
 //  TODO (6) Create a constructor that calls super and passes the name of this class
 //  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+public class SunshineSyncIntentService extends IntentService {
+
+    public SunshineSyncIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
