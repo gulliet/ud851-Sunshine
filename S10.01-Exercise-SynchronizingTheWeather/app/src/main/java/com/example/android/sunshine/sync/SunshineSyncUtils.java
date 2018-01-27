f7900bb1cf51e116149c4e1a7036a6c40e8c95cd
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-27 11:56:13 +0100

    Create a public static void method called startImmediateSync

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,9 +1,12 @@
 package com.example.android.sunshine.sync;
 
 // completed (9) Create a class called SunshineSyncUtils
-//  TODO (10) Create a public static void method called startImmediateSync
+//  completed (10) Create a public static void method called startImmediateSync
 //  TODO (11) Within that method, start the SunshineSyncIntentService
 
 public class SunshineSyncUtils {
 
+    public static void startImmediateSync() {
+
+    }
 }
