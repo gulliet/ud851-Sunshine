c5a64bbf4a5ab9b23d8814b1c643bc19da7c6657
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-27 11:31:47 +0100

    Create a class called SunshineSyncTask

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,15 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
+package com.example.android.sunshine.sync;
+
+//  completed (1) Create a class called SunshineSyncTask
+//  completed (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
 //      TODO (3) Within syncWeather, fetch new weather data
 //      TODO (4) If we have valid results, delete the old data and insert the new
+
+import android.content.Context;
+
+public class SunshineSyncTask {
+
+    synchronized public static void syncWeather(Context context) {
+
+    }
+}
